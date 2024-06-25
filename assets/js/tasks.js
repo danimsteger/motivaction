eac0efbbabe1f23ed7cbdb46c0e618a62559de2c
danimsteger <daniellestegertutoring@gmail.com>
2024-06-25 14:29:08 -0400

    added modal content, began working on modal js functions

--- assets/js/tasks.js
+++ assets/js/tasks.js
@@ -1,3 +1,14 @@
 const today = dayjs();
 
 $("#current-date").text(today.format("dddd, MMMM DD"));
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   function openModal($el) {
+//     $el.classList.add("is-active");
+//   }
+//   function closeModal($el) {
+//     $el.classList.remove("is-active");
+//   }
+// });
+
+// $('.js-modal-trigger')
